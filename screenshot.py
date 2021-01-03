a82178a1d4f60b9ba71b4e9ec14610c184c333d3
Logesh.S <76871611+Everything-with-python@users.noreply.github.com>
2021-01-03 11:07:15 +0530

    Add files via upload

---
+++ screenshot.py
@@ -0,0 +1,5 @@
+import pyautogui
+ss = pyautogui.screenshot()
+ss.save("screenshot 2.png")     # u can save it in any format like ( .jpg  or  .png )
+
+# u can save the screenshot name by giving within the double quotes like the last line of the code
