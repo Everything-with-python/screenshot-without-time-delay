cd5bef989641cd1919a98a16b9b9a1d6fe10a308
Logesh.S <76871611+Everything-with-python@users.noreply.github.com>
2021-01-03 11:26:16 +0530

    Update screenshot with time delay.py

--- screenshot with time delay.py
+++ screenshot with time delay.py
@@ -5,26 +5,6 @@ time.sleep(3)
 ss = pyautogui.screenshot()
 ss.save("screenshot 2.png")
 
-
-        # u can change the time (in seconds) for ur wish
-        # here i used to wait for 3sec, so after 3sec it will take a screenshot
-
-        # u can save the screenshot name by giving within the double quotes like the last line of the code
-        # u can save it in any format like ( .jpg  or  .png )
-
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                  
+# u can change the time (in seconds) for ur wish
+# here i used to wait for 3sec, so after 3sec it will take a screenshot
+# u can save the screenshot name by giving within the double quotes like the last line of the code
